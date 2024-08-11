6a9c742eb0ecea54a73cf2dd99401469241bf502
rukhman <aleksei.rukhmanov@gmail.com>
2024-08-11 13:40:49 +0300

    2

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MatIconModule, MatSidenavModule, MatButtonModule],
+  imports: [MatIconModule, MatSidenavModule, MatButtonModule, CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  windowWith = window.innerWidth;
+  ngOnInit(): void {
+    console.log(window.innerWidth);
+  }
   closeSideNav(drawer: any) {
     drawer.close();
   }
